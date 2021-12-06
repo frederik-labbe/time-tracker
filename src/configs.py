03fe2f577d5d028f3eac8d50304f43ef162776da
frederik-labbe <frederik.labbe@gmail.com>
2021-12-06 02:51:48 -0500

    Increase write interval again

--- src/configs.py
+++ src/configs.py
@@ -1,6 +1,6 @@
 AREAS = ['Area 1', 'Area 2', 'Area 3']
 REASONS = ['Reason 1', 'Reason 2', 'Reason 3']
-WRITE_INTERVAL_SEC = 10
+WRITE_INTERVAL_SEC = 30
 OUTPUT_FOLDER = 'data'
 WORKING_HOUR_START = 8
 WORKING_HOUR_END = 17
