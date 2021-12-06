8d6d341c0edcb2f1206edb65f1b44e2631453b6d
frederik-labbe <frederik.labbe@gmail.com>
2021-12-06 00:16:31 -0500

    Fix default working hours

--- src/configs.py
+++ src/configs.py
@@ -2,5 +2,5 @@ AREAS = ['Area 1', 'Area 2', 'Area 3']
 REASONS = ['Reason 1', 'Reason 2', 'Reason 3']
 WRITE_INTERVAL_SEC = 2
 OUTPUT_FOLDER = 'data'
-WORKING_HOUR_START = 3
+WORKING_HOUR_START = 8
 WORKING_HOUR_END = 17
